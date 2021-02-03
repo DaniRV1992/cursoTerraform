ca4a48dbc1cde40eb8a2659370d0f7853f4c08fe
Ubuntu <ubuntu@ip-172-31-7-4.eu-west-1.compute.internal>
2021-02-03 17:16:36 +0000

    Nginx map map string

--- ejemplo2/main.tf
+++ ejemplo2/main.tf
@@ -20,3 +20,17 @@ resource "docker_container" "contenedor-nginx" {
         external = 8080
     }
 }
+
+resource "docker_container" "contenedores-map-nginx" {
+    for_each = var.contenedores_custom
+    name  = each.key
+    image = docker_image.imagen-nginx.latest
+    ports {
+        internal = 80
+        external = each.value["puerto"]
+    }
+    volumes {
+        host_path      = each.value["host_path"]
+        container_path = each.value["container_path"]
+    }
+}
--- ejemplo2/variables.tf
+++ ejemplo2/variables.tf
@@ -9,3 +9,39 @@ variable "version_imagen" {
     type = string
     default = "latest"
 }
+
+# variable "contenedores" {
+#     description = "contenedores"
+#     type = list(object(string))
+#     default = [
+#         {
+#             nombre = "contenedorA"
+#             puerto = "8090"
+#             volumen = {
+#                 host_path = "/home/ubuntu/environment/cursoTerraform"
+#                 container_path = "/cursoTerraform"
+#             }
+#         },
+#         {
+#             nombre = "contenedorB"
+#             puerto = "8091"
+#         }
+#     ]
+# }
+
+variable "contenedores_custom" {
+    description = "Contendores nginx"
+    type        = map(map(string))
+    default     = {
+        VERDE  = {
+            puerto         = 8090    
+            host_path      = "/home/ubuntu/environment/cursoTerraform"
+            container_path = "/cursoTerraform"
+        }
+        AMARILLO  = {
+            puerto         = 8091
+            container_path = "/ivan"
+            host_path      = "/home/ubuntu/environment/ivan"
+        }
+    }
+}
